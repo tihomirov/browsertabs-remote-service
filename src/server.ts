e53ac15fe42dc3e449e1abc448e0b5078943c836
Anton <antontikhomirov93@gmail.com>
2023-05-15 21:13:57 +0300

    Add bbroadcasting

--- src/server.ts
+++ src/server.ts
@@ -15,10 +15,25 @@ wss.on('connection', (ws: WebSocket) => {
 
     //connection is up, let's add a simple simple event
     ws.on('message', (message: string) => {
-
         //log the received message and send it back to the client
         console.log('received: %s', message);
-        ws.send(`Hello, you sent -> ${message}`);
+
+        const broadcastRegex = /^broadcast\:/;
+
+        if (broadcastRegex.test(message)) {
+            message = message.replace(broadcastRegex, '');
+
+            //send back the message to the other clients
+            wss.clients
+                .forEach(client => {
+                    if (client != ws) {
+                        client.send(`Hello, broadcast message -> ${message}`);
+                    }    
+                });
+            
+        } else {
+            ws.send(`Hello, you sent -> ${message}`);
+        }
     });
 
     //send immediatly a feedback to the incoming connection    
